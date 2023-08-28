4b58ed67f24057ac155192ffa3a2788c5d8f7836
IlonaBorkovska <borkovska.io@gmail.com>
2023-08-28 22:25:41 +0300

    setup linter, prettier, lint-staged, husky

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,12 @@ import 'dotenv/config';
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  }),
+);
+
 app.use(express.json());
 
 app.use('/', (req, res) => {
