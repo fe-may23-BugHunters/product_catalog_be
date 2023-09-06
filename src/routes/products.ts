93186f6498556bcef87c996566d5f649d3b07b4c
Anatolii Lykhohodenko <anatollykh10@gmail.com>
2023-09-06 12:23:51 +0300

    add-routes

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -3,9 +3,9 @@ import express from 'express';
 
 export const router = express.Router();
 
-router.get('/:category', ProductController.getAll);
+router.get('/', ProductController.getAll);
 router.get('/:productId', ProductController.getById);
-router.get('/:productId/recommended', ProductController.getById);
-router.get('/recommended', ProductController.getById);
-router.get('/new', ProductController.getById);
-router.get('/discount', ProductController.getById);
+router.get('/:productId/recommended', ProductController.getRandom10);
+router.get('/recommended', ProductController.getRandom10);
+router.get('/new', ProductController.getRandom10);
+router.get('/discount', ProductController.getRandom10);
