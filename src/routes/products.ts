6d3f374d080d1b6526d9e24aa79ff388b97ea654
Valentyna <vabo1108@gmail.com>
2023-09-06 16:45:43 +0200

    add search-endpoint

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -4,6 +4,7 @@ import express from 'express';
 export const router = express.Router();
 
 router.get('/', ProductController.getAll);
+router.get('/search', ProductController.searchProducts);
 router.get('/recommended', ProductController.getRandom10);
 router.get('/variants', ProductController.getByVariant);
 router.get('/new', ProductController.getRandom10);
